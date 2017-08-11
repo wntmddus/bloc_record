3aabc6dbcf9d1eec15c1f0e15c37a0324c6ff1b1
Seung Yeon Joo <wntmddussla@hotmail.com>
2017-08-10 22:34:18 -0700

    7 Associations

--- lib/bloc_record.rb
+++ lib/bloc_record.rb
@@ -1,9 +1,14 @@
 module BlocRecord
-   def self.connect_to(filename)
+   def self.connect_to(filename, database)
      @database_filename = filename
+     @database = database
    end
 
    def self.database_filename
      @database_filename
+   end
+
+   def self.database
+     @database
    end
  end
